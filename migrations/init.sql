4dba433ee641040c89dc6a7f79d2ba3ed3535cb5
Ahmad Hanjoura <ahmad_hanjoura@hotmail.com>
2019-02-08 15:26:35 -0700

    created populate migration file and queries.go

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,12 +1,38 @@
 -- +goose Up
 CREATE TABLE users (
-    uid int is NOT NULL,
-    first_name text is NOT NULL,
-    last_name text is NOT NULL,
-    PRIMARY KEY(uid)
+    uid SERIAL PRIMARY KEY,
+    first_name text NOT NULL,
+    last_name text NOT NULL
 );
 
+CREATE TABLE presentations (
+    pid SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    date timestamp NOT NULL
+);
+
+CREATE TABLE questions (
+    qid SERIAL PRIMARY KEY,
+    type text NOT NULL,
+    question text NOT NULL
+);
+
+CREATE TABLE answers (
+    aid SERIAL PRIMARY KEY,
+    qid integer REFERENCES questions(qid),
+    type text REFERENCES questions(type),
+    answer text
+);
+
+CREATE TABLE presenters (
+    uid integer REFERENCES users(uid),
+    pid integer REFERENCES presentations(pid)
+);
 
 
 -- +goose Down
 DROP TABLE users;
+DROP TABLE presentations;
+DROP TABLE questions;
+DROP TABLE answers;
+DROP TABLE presenters;
